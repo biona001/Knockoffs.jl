a513c2db9e28cbc40db5cf5cc9be6de9b2a93df2
Documenter.jl <documenter@juliadocs.github.io>
2021-09-08 04:46:21 +0000

    build based on 56ada65

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Knockoffs","category":"page"},{"location":"#Knockoffs","page":"Home","title":"Knockoffs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Knockoffs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Knockoffs]","category":"page"}]
+}
