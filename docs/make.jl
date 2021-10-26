69225d919a4ea77d61f2f4ed845cd46362be352f
Unknown <benchu99@hotmail.com>
2021-10-26 16:23:30 -0700

    update make and index.md files to properly display docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,21 @@
 using Knockoffs
 using Documenter
 
-DocMeta.setdocmeta!(Knockoffs, :DocTestSetup, :(using Knockoffs); recursive=true)
-
-makedocs(;
-    modules=[Knockoffs],
-    authors="Benjamin Chu <benchu99@hotmail.com> and contributors",
-    repo="https://github.com/biona001/Knockoffs.jl/blob/{commit}{path}#{line}",
-    sitename="Knockoffs.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://biona001.github.io/Knockoffs.jl",
-        assets=String[],
-    ),
-    pages=[
+makedocs(
+    sitename = "Knockoffs.jl",
+    format = Documenter.HTML(),
+    modules = [Knockoffs],
+    pages = [
         "Home" => "index.md",
         "HMM Knockoffs" => "man/hmm.md",
         "API" => "man/api.md",
-    ],
+    ]
 )
 
-deploydocs(;
-    repo="github.com/biona001/Knockoffs.jl",
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo   = "github.com/biona001/Knockoffs.jl.git",
+    target = "build"
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,8 +1,4 @@
-```@meta
-CurrentModule = Knockoffs
-```
-
-# Knockoffs
+# Knockoffs.jl
 
 This is a Julia implementation of the [knockoff filter](https://web.stanford.edu/group/candes/knockoffs/). The knockoff filter is a general framework for controlling the false discovery rate when performing variable selection. As the name suggests, the knockoff filter operates by manufacturing knockoff variables that are cheap — their construction does not require collecting any new data — and are designed to mimic the correlation structure found within the original variables. The knockoffs serve as negative controls and they allow one to identify the truly important predictors, while controlling the false discovery rate (FDR) — the expected fraction of false discoveries among all discoveries.
 
@@ -16,7 +12,6 @@ using Pkg
 pkg"add https://github.com/biona001/Knockoffs.jl"
 ```
 This package supports Julia `v1.6`+.
-
 
 ## Manual Outline
 
