06eb0272c79e0c4db4ad2130bfb348c1ab024ff3
Unknown <benchu99@hotmail.com>
2021-12-28 22:22:30 -0800

    compute 10 PCs

--- scripts/compute_ukb_subset_pca.jl
+++ scripts/compute_ukb_subset_pca.jl
@@ -6,5 +6,5 @@ for chr in 1:22
     println("computing PCA for chr $chr")
     plinkname = "/scratch/users/bbchu/ukb/subset/ukb.10k.chr$chr"
     outfile = "/scratch/users/bbchu/ukb/subset_pca/ukb.10k.chr$chr."
-    run(`$propca_exe -g $plinkname -o $outfile -nt $(Threads.nthreads()) -seed $seed`)
+    run(`$propca_exe -g $plinkname -k 10 -o $outfile -nt $(Threads.nthreads()) -seed $seed`)
 end
