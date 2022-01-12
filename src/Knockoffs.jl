026b838af48c9469f9e0649cc2220c5f06846baf
Unknown <benchu99@hotmail.com>
2022-01-11 16:41:11 -0800

    update Project.toml

--- src/Knockoffs.jl
+++ src/Knockoffs.jl
@@ -5,11 +5,16 @@ using Statistics
 using Convex
 using SCS
 using SnpArrays
+using Combinatorics
+using DelimitedFiles
+using ProgressMeter
 import Base: eltype, getindex, size
 
 export fixed_knockoffs, modelX_gaussian_knockoffs, normalize_col!,
     coefficient_diff, threshold, extract_beta, extract_combine_beta,
-    partition, rapid, snpknock2, decorrelate_knockoffs
+    partition, rapid, snpknock2, decorrelate_knockoffs, process_fastphase_output, 
+    get_haplotype_transition_matrix, get_genotype_transition_matrix, 
+    get_initial_probabilities
 
 include("struct.jl")
 include("fixed.jl")
