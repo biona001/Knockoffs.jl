6216c5d01b70d8a86c3c41108c3aca0e2fef2175
Unknown <benchu99@hotmail.com>
2022-01-16 14:31:14 -0800

    export hmm functions

--- src/Knockoffs.jl
+++ src/Knockoffs.jl
@@ -11,10 +11,15 @@ import Base: eltype, getindex, size
 
 export fixed_knockoffs, modelX_gaussian_knockoffs, normalize_col!,
     coefficient_diff, threshold, extract_beta, extract_combine_beta,
-    partition, rapid, snpknock2, decorrelate_knockoffs
-    # process_fastphase_output, 
-    # get_haplotype_transition_matrix, get_genotype_transition_matrix, 
-    # get_initial_probabilities
+    partition, rapid, snpknock2, decorrelate_knockoffs,
+    process_fastphase_output, 
+    # functions for hmm
+    get_haplotype_transition_matrix, get_genotype_transition_matrix, 
+    get_initial_probabilities,
+    GenotypeState, MarkovChainTable, pair_to_index, index_to_pair,
+    hmm_knockoff, forward_backward_sampling, forward_backward_sampling!,
+    form_emission_prob_matrix, get_genotype_emission_probabilities,
+    get_haplotype_emission_probabilities
 
 include("struct.jl")
 include("fixed.jl")
