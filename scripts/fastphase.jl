1011ebe3487c541ae920490b3e460ad2afcfb294
Unknown <benchu99@hotmail.com>
2022-04-08 12:47:51 -0700

    remove scripts for prs

--- scripts/fastphase.jl
+++
@@ -1,8 +0,0 @@
-using SnpArrays
-using Knockoffs
-plinkname = "/scratch/users/bbchu/ukb_SHAPEIT/subset/ukb.10k.chr10"
-xdata = SnpData(plinkname)
-@time r, θ, α = fastphase(xdata; n=1000, out="ukb_chr10_n1000")
-
-# for reading aggregated results later
-# r, θ, α = process_fastphase_output(pwd(), T=1, extension="ukb_chr10_n1000")
